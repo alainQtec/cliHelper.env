2cf758c356f98f47c78c1a824c8f388d7f45d734
Alain Herve <alainQtec@gmail.com>
2024-09-21 14:28:12 +0200

    Update dotEnv.Config.psm1

--- Private/dotEnv.Config/dotEnv.Config.psm1
+++ Private/dotEnv.Config/dotEnv.Config.psm1
@@ -1,4 +1,5 @@
-﻿class CfgList {
+﻿#!/usr/bin/env pwsh
+class CfgList {
   CfgList() {
     $this.PsObject.properties.add([psscriptproperty]::new('Count', [scriptblock]::Create({ ($this | Get-Member -Type *Property).count - 2 })))
     $this.PsObject.properties.add([psscriptproperty]::new('Keys', [scriptblock]::Create({ ($this | Get-Member -Type *Property).Name.Where({ $_ -notin ('Keys', 'Count') }) })))
@@ -160,5 +161,40 @@ class EnvCfg : CfgList {
     $this.PrivateCertFile = [IO.FileInfo][IO.Path]::Combine($CertPath, "$($this.CertName).key.pem");
     $this.PublicCertFile = [IO.FileInfo][IO.Path]::Combine($CertPath, "$($this.CertName).cert.pem")
     $this.PfxFile = [IO.FileInfo][IO.Path]::Combine($CertPath, "$($this.CertName).pfx")
+  }
+}
+
+function Set-dotEnvConfig {
+  # .SYNOPSIS
+  #     Supposed to run on-time, during module initial setup. It prepares Credentials to use when securing environment variables on local machine.
+  # .DESCRIPTION
+  #     Generates a secure hashed credential file and configuration for the dotEnv module.
+  #     Has options to choose between DPAPI or AES encryption modes.
+  #     DPAPI is more secure but requires to be run by the same user account on the same windows machine.
+  #     AES is also secure but can be used when service account cannot be used to run in interactive mode.
+  # .NOTES
+  #     Information or caveats about the function e.g. 'This function is not supported in Linux'
+  # .LINK
+  #     Specify a URI to a help page, this will show when Get-Help -Online is used.
+  # .EXAMPLE
+  #     Set-dotEnvConfig
+  #     Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+  [CmdletBinding(SupportsShouldProcess = $true)]
+  [Alias("Initialize-dotEnv")]
+  param (
+
+  )
+
+  begin {
+  }
+
+  process {
+    if ($PSCmdlet.ShouldProcess("Localhost", "Initialize dotEnv")) {
+      # do stuff here
+      # Write-Host "Hello from Private/dotEnv.Config/Set-dotEnvConfig" -f Green
+    }
+  }
+
+  end {
   }
 }
