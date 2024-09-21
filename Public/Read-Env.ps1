df9ee4e71ef0b70ee5dd6df8a0f89447d50aac6b
Alain Herve <alainQtec@gmail.com>
2024-09-21 17:37:59 +0200

    Create Read-Env.ps1

---
+++ Public/Read-Env.ps1
@@ -0,0 +1,27 @@
+﻿function Read-Env {
+  <#
+  .SYNOPSIS
+    Reads environment Variable(s) from a .env file.
+  .DESCRIPTION
+    Same as: Get-Env -FromFilesOnly
+  .LINK
+    Specify a URI to a help page, this will show when Get-Help -Online is used.
+  .EXAMPLE
+    Read-Env ./.env
+  #>
+  [CmdletBinding(DefaultParameterSetName = "path")]
+  param (
+    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'path')]
+    [ValidateNotNullOrEmpty()]
+    [string]$Path,
+    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'file')]
+    [ValidateNotNullOrEmpty()]
+    [IO.FileInfo]$source
+  )
+  end {
+    if ($PSCmdlet.ParameterSetName -eq "path") {
+      return Get-Env -FromFilesOnly -source ([IO.FileInfo]::new($source))
+    }
+    return Get-Env -FromFilesOnly -source $source
+  }
+}
