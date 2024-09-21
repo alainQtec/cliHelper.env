d56d379c230b5d9da9c4a0ef0ca426daa4fa445a
Alain Herve <alainQtec@gmail.com>
2024-09-21 17:37:48 +0200

    Create Write-Env.ps1

---
+++ Public/Write-Env.ps1
@@ -0,0 +1,22 @@
+﻿function Write-Env {
+  # .SYNOPSIS
+  #   same as: Add-Env -ToFilesOnly
+  # .DESCRIPTION
+  #   Write environment Variable(s) to a .env file, but does not set it.
+  [CmdletBinding(DefaultParameterSetName = "path")]
+  param (
+    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'path')]
+    [ValidateNotNullOrWhiteSpace()]
+    [string]$Path,
+    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'file')]
+    [ValidateNotNullOrEmpty()]
+    [IO.FileInfo]$source
+  )
+
+  end {
+    if ($PSCmdlet.ParameterSetName -eq "path") {
+      return Add-Env -ToFilesOnly -source ([IO.FileInfo]::new($source))
+    }
+    return Add-Env -ToFilesOnly
+  }
+}
