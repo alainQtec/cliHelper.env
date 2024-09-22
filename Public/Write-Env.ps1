b9c25732bcf9d0c8ee84b43b72e92369ae55056d
Alain Herve <alainQtec@gmail.com>
2024-09-22 20:36:46 +0200

    Update Write-Env.ps1
    refactor . . .

--- Public/Write-Env.ps1
+++ Public/Write-Env.ps1
@@ -1,22 +1,29 @@
 ﻿function Write-Env {
   # .SYNOPSIS
-  #   same as: Add-Env -ToFilesOnly
+  #   same as: Add-Env -outFile
   # .DESCRIPTION
   #   Write environment Variable(s) to a .env file, but does not set it.
   [CmdletBinding(DefaultParameterSetName = "path")]
   param (
-    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'path')]
+    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'kv')]
     [ValidateNotNullOrWhiteSpace()]
-    [string]$Path,
-    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'file')]
-    [ValidateNotNullOrEmpty()]
-    [IO.FileInfo]$source
+    [string]$Name,
+
+    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'entries')]
+    [dotEntry[]]$Entries,
+
+    [Parameter(Mandatory = $true, Position = 2, ParameterSetName = 'kv')]
+    [string]$Value,
+    [Parameter(Mandatory = $true, Position = 1, ParameterSetName = '__AllparameterSets')]
+    [ValidateNotNullOrWhiteSpace()]
+    [string]$Path
   )
 
   end {
-    if ($PSCmdlet.ParameterSetName -eq "path") {
-      return Add-Env -ToFilesOnly -source ([IO.FileInfo]::new($source))
+    if ($PSCmdlet.ParameterSetName -eq "kv") {
+      return [dotEnv]::Update($Path, $Name, $Value)
+    } else {
+      return [dotEnv]::Update($Entries, $Name, $Value)
     }
-    return Add-Env -ToFilesOnly
   }
 }
