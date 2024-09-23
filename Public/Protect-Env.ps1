12809959566794d945df75dbd69747bb33c45ddb
Alain Herve <alainQtec@gmail.com>
2024-09-23 23:09:12 +0200

    Update Protect-Env.ps1

--- Public/Protect-Env.ps1
+++ Public/Protect-Env.ps1
@@ -10,7 +10,7 @@
   # .LINK
   #     Initialize-EnvConfig
   # .LINK
-  #     Protect-Env
+  #     https://github.com/alainQtec/dotEnv/Public/Protect-Env.ps1
   # .EXAMPLE
   #     Protect-Env NEXT_PUBLIC_SUPER_SECRET_KEY
   #     Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
