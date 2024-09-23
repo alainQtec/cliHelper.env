d58136cf2d419eba7e5e830553253c4e579d8920
Alain Herve <alainQtec@gmail.com>
2024-09-23 23:09:17 +0200

    Update Remove-Env.ps1

--- Public/Remove-Env.ps1
+++ Public/Remove-Env.ps1
@@ -1,68 +1,26 @@
 ﻿function Remove-Env {
   # .SYNOPSIS
   #   Removes a persistent environment variable.
-
   # .DESCRIPTION
   #   Removes an environment variable
   #   with the specified name and value. The variable can be scoped either to
-  #   the User or to the Machine. If Machine level scoping is specified, the
-  #   command is elevated to an administrative session.
-
-  # .INPUTS
-  #   None
-
-  # .OUTPUTS
-  #   null
-
-  # .PARAMETER VariableName
-  #   The name or key of the environment variable to remove.
-
-  # .PARAMETER VariableType
-  #   Specifies whether this variable is at either the individual User level
-  #   or at the Machine level.
-
-  # .PARAMETER IgnoredArguments
-  #   Allows splatting with arguments that do not apply. Do not use directly.
-
+  #   the User or to the Machine.
   # .EXAMPLE
   #   >
-  #   # Remove an environment variable
-  #   Remove-Env -VariableName 'bob'
-
-  # .EXAMPLE
-  #   >
-  #   # Remove an environment variable from Machine
-  #   Remove-Env -VariableName 'bob' -VariableType 'Machine'
-
-  # .NOTES
-  #   Administrative Access Required when `-VariableType 'Machine'.`
+  #   Remove-Env -Name 'bob' -VariableType 'Machine'
   [CmdletBinding(SupportsShouldProcess = $true)]
   param(
     [parameter(Mandatory = $true, Position = 0)]
-    [string]$variableName,
+    [string]$Name,
 
     [parameter(Mandatory = $false, Position = 1)]
-    [System.EnvironmentVariableTarget]$variableType = [System.EnvironmentVariableTarget]::User,
-
-    [parameter(ValueFromRemainingArguments = $true)]
-    [Object[]]$ignoredArguments
+    [System.EnvironmentVariableTarget]$Scope = "Process"
   )
 
-  # Log Invocation  and Parameters used. $MyInvocation, $PSBoundParameters
-  $fxn = ('[' + $MyInvocation.MyCommand.Name + ']')
-
-  [bool]$IsAdmin = $((New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator));
-  if ($variableType -eq [System.EnvironmentVariableTarget]::Machine) {
-    if ($IsAdmin) {
-      Add-Env -Name $variableName -Value $null -Scope $variableType
-    } else {
-      Write-Warning "$fxn : [!]  It seems You're not Admin [!] "
-      $psArgs = "Add-Env -Name `'$variableName`' -Value $null -variableType `'$variableType`'"
-      Start-ProcessAsAdmin "$psArgs"
+  process {
+    if ($PSCmdlet.ShouldProcess("Target", "Operation")) {
+      Add-Env -Name $Name -Value $null -Scope $Scope -Force
+      # Set-Content Env:/$Name $null
     }
-  } else {
-    Add-Env -Name $variableName -Value $null -Scope $variableType
   }
-
-  Set-Content Env:/$variableName $null
 }
